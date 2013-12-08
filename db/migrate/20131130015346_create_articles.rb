494001b0f15ca23840f06e420b94f61123d4efe8
Ronnie Brown <ronnievoice@gmail.com>
2013-12-08 12:42:15 -0800

    update database with column

--- db/migrate/20131130015346_create_articles.rb
+++ db/migrate/20131130015346_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration
     create_table :articles do |t|
       t.string :title
       t.text :content
-  	  t.string :name
+  	 
       t.timestamps
     end
   end
