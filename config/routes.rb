0b6350b84054760bb60b8afe9dfacb36a1ba6f47
Ronnie Brown <ronnievoice@gmail.com>
2013-12-07 21:02:12 -0800

    add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Wikiful::Application.routes.draw do
+  resources :users
+
   get "categories/index"
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
